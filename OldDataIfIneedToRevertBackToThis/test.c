594cf5b1f262844db3950fd87174527555a3cd1c
Vivaan Malik <malikvivaan@gmail.com>
2025-11-07 17:53:50 +0530

    remove foler

--- OldDataIfIneedToRevertBackToThis/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include "../include/test.h"
-
-void print(const char *text) {
-    for (int i =0; i < strlen(text); i++) {
-        printf("%c", text[i]);
-    }
-}
--- OldDataIfIneedToRevertBackToThis/test.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_H
-#define TEST_H
-
-#define HELLO_WORLD "Hello World!\n"
-
-void print(const char *);
-
-#endif
